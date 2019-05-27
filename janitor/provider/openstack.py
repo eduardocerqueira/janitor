e3d68df002af42f90597012fe56e8924f5137b4e
patchkez <email@email.com>
2019-05-27 13:33:47 +0200

    add keystone v3 support + new click argument

--- janitor/provider/openstack.py
+++ janitor/provider/openstack.py
@@ -16,31 +16,46 @@
 import glanceclient.v2.client as glclient
 from os import getenv
 from novaclient.client import Client
-from keystoneauth1.identity import v2
 from keystoneauth1 import session
 from neutronclient.v2_0.client import Client as neutronclient
 from sys import exit
 
 
 class Credentials(object):
-    def __init__(self, openrc=None):
+    def __init__(self, openrc=None, keystone='v3'):
         self.openrc_path = openrc
         self.osp_username = getenv("OS_USERNAME")
         self.osp_password = getenv("OS_PASSWORD")
-        self.osp_tenant = getenv("OS_TENANT_NAME")
         self.osp_auth_url = getenv("OS_AUTH_URL")
-        self.osp_auth_version = '3.x_password'
-        self.osp_service_type = 'compute'
         self.osp_service_region = getenv("OS_REGION_NAME")
+        if keystone == 'v2':
+            self.osp_tenant = getenv("OS_TENANT_NAME")
+            self.osp_auth_version = '3.x_password'
+            self.osp_service_type = 'compute'
+
+        if keystone == 'v3':
+            self.osp_project_id = getenv("OS_PROJECT_ID")
+            self.osp_project_name = getenv("OS_PROJECT_NAME")
+            self.osp_user_domain_name = getenv("OS_USER_DOMAIN_NAME")
+            self.osp_project_domain_id = getenv("OS_PROJECT_DOMAIN_ID")
+            self.ospidentity_api_version = getenv("OS_IDENTITY_API_VERSION")
+
 
         # file overrides env variable
         if self.openrc_path:
             openrc = self.read_openrc()
             self.osp_username = openrc["OS_USERNAME"]
             self.osp_password = openrc["OS_PASSWORD"]
-            self.osp_tenant = openrc["OS_TENANT_NAME"]
             self.osp_auth_url = openrc["OS_AUTH_URL"]
             self.osp_service_region = openrc["OS_REGION_NAME"]
+            if keystone == 'v2':
+                self.osp_tenant = openrc["OS_TENANT_NAME"]
+            if keystone == 'v3':
+                self.osp_project_id = openrc["OS_PROJECT_ID"]
+                self.osp_project_name = openrc["OS_PROJECT_NAME"]
+                self.osp_user_domain_name = openrc["OS_USER_DOMAIN_NAME"]
+                self.osp_project_domain_id = openrc["OS_PROJECT_DOMAIN_ID"]
+                self.osp_identity_api_version = openrc["OS_IDENTITY_API_VERSION"]
 
         if not self.osp_username:
             raise Exception("Missing OS_USERNAME")
@@ -48,14 +63,27 @@ class Credentials(object):
         if not self.osp_password:
             raise Exception("Missing OS_PASSWORD")
 
-        if not self.osp_tenant:
-            raise Exception("Missing OS_TENANT_NAME")
-
         if not self.osp_auth_url:
             raise Exception("Missing OS_AUTH_URL")
 
         if not self.osp_service_region:
             raise Exception("Missing OS_REGION_NAME")
+
+        if keystone == 'v2':
+            if not self.osp_tenant:
+                raise Exception("Missing OS_TENANT_NAME")
+
+        if keystone == 'v3':
+            if not self.osp_project_id:
+                raise Exception("Missing OS_PROJECT_ID")
+            if not self.osp_project_name:
+                raise Exception("Missing OS_PROJECT_NAME")
+            if not self.osp_user_domain_name:
+                raise Exception("Missing OS_USER_DOMAIN_NAME")
+            if not self.osp_project_domain_id:
+                raise Exception("Missing OS_PROJECT_DOMAIN_ID")
+            if not self.osp_identity_api_version:
+                raise Exception("Missing OS_IDENTITY_API_VERSION")
 
     def read_openrc(self):
         openrc = {}
@@ -73,13 +101,24 @@ class Credentials(object):
 
 
 class OpenstackSDK(object):
-    def __init__(self, openrc=None):
+    def __init__(self, openrc=None, keystone='v3'):
         self.creds = Credentials(openrc)
-        self.auth = v2.Password(auth_url="%s/%s"
-                                % (self.creds.osp_auth_url, "/v2.0"),
-                                username=self.creds.osp_username,
-                                password=self.creds.osp_password,
-                                tenant_name=self.creds.osp_tenant)
+        if keystone == 'v2':
+            from keystoneauth1.identity import v2
+            self.auth = v2.Password(auth_url="%s/%s"
+                                    % (self.creds.osp_auth_url, "/v2.0"),
+                                    username=self.creds.osp_username,
+                                    password=self.creds.osp_password,
+                                    tenant_name=self.creds.osp_tenant)
+        if keystone == 'v3':
+            from keystoneauth1.identity import v3
+            self.auth = v3.Password(auth_url=self.creds.osp_auth_url,
+                                    project_id=self.creds.osp_project_id,
+                                    project_name=self.creds.osp_project_name,
+                                    user_domain_name=self.creds.osp_user_domain_name,
+                                    project_domain_id=self.creds.osp_project_domain_id,
+                                    username=self.creds.osp_username,
+                                    password=self.creds.osp_password)
         self.session = session.Session(auth=self.auth)
         self.nova = Client('2', session=self.session)
         self.glance = glclient.Client('2', session=self.session)
